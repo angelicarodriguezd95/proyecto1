7b63ccc8c483206b1edaf9985ea0c752ccddcad4
angelicarodriguezd95 <118750418+angelicarodriguezd95@users.noreply.github.com>
2022-11-21 11:01:12 -0500

    Add files via upload

---
+++ js/ejercicio2_13.js
@@ -0,0 +1,28 @@
+
+ let empl1;
+ let empl2;
+ let empl3;
+ 
+ let s1;
+ let s2;
+ let s3;
+
+ s1 = parseInt(prompt("Ingresar el sueldo del primer empleado"));
+ s2 = parseInt(prompt("Ingresar el sueldo del segunto empleado"));
+ s3 = parseInt(prompt("Ingresar el sueldo del tercer empleado"));
+
+
+empl1=  0.1*s1 + s1;
+empl2= 0.12*s2 + s2;
+empl3= 0.15*s3 + s3;
+
+document.write("<br>El salario del empleado 1 es: s/ "  + empl1);
+document.write("<br>El salario del empleado 2 es: s/ "  + empl2);
+document.write("<br>El salario del empleado 3 es: s/ "  + empl3);
+
+
+
+
+
+
+
